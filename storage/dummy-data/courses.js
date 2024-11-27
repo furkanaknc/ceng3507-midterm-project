1228197a0bd659924087a3de006b111d67674adc
Furkan Akıncı <furkanaknc55@gmail.com>
2024-11-27 19:15:03 +0300

    feat: add dummy data

---
+++ storage/dummy-data/courses.js
@@ -0,0 +1,42 @@
+export const dataOfCourses = {
+    courses: [
+        {
+            name: "WEB DEVELOPMENT",
+            gradingScale: "10-point",
+            students: [
+                { id: "1", GPA: "83.00", letterGrade: "B" },
+                { id: "3", GPA: "48.00", letterGrade: "F" }
+            ],
+        },
+        {
+            name: "MOBILE APPLICATION DEVELOPMENT",
+            gradingScale: "7-point",
+            students: [
+                { id: "1", GPA: "72.50", letterGrade: "C" },
+                { id: "3", GPA: "93.50", letterGrade: "A" }
+            ],
+        },
+        {
+            name: "DATABASE MANAGEMENT SYSTEM",
+            gradingScale: "10-point",
+            students: [
+                { id: "2", GPA: "35.00", letterGrade: "F" }
+            ],
+        },
+        {
+            name: "COMPUTER NETWORK",
+            gradingScale: "7-point",
+            students: [
+                { id: "2", GPA: "96.50", letterGrade: "A" }
+            ],
+        },
+        {
+            name: "DATA STRUCTURES",
+            gradingScale: "10-point",
+            students: [
+                { id: "2", GPA: "95.00", letterGrade: "A" },
+                { id: "3", GPA: "48.50", letterGrade: "F" }
+            ],
+        }
+    ]
+};
---
+++ storage/dummy-data/students.js
@@ -0,0 +1,65 @@
+export const dataOfStudents = {
+    students: [
+        {
+            id: "1",
+            name: "James",
+            surname: "Wilson",
+            courses: [
+                {
+                    courseName: "WEB DEVELOPMENT",
+                    midtermScore: 78,
+                    finalScore: 88,
+                },
+                {
+                    courseName: "MOBILE APPLICATION DEVELOPMENT",
+                    midtermScore: 68,
+                    finalScore: 77,
+                },
+            ],
+        },
+        {
+            id: "2",
+            name: "David",
+            surname: "Jones",
+            courses: [
+                {
+                    courseName: "DATABASE MANAGEMENT SYSTEM",
+                    midtermScore: 45,
+                    finalScore: 25,
+                },
+                {
+                    courseName: "COMPUTER NETWORK",
+                    midtermScore: 95,
+                    finalScore: 98,
+                },
+                {
+                    courseName: "DATA STRUCTURES",
+                    midtermScore: 98,
+                    finalScore: 92,
+                },
+            ],
+        },
+        {
+            id: "3",
+            name: "Robert",
+            surname: "Taylor",
+            courses: [
+                {
+                    courseName: "WEB DEVELOPMENT",
+                    midtermScore: 55,
+                    finalScore: 45,
+                },
+                {
+                    courseName: "MOBILE APPLICATION DEVELOPMENT",
+                    midtermScore: 95,
+                    finalScore: 92,
+                },
+                {
+                    courseName: "DATA STRUCTURES",
+                    midtermScore: 52,
+                    finalScore: 45,
+                },
+            ],
+        }
+    ]
+};
